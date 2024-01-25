75e5356d58d7fb5ee6da0fd12e6dfd256432c7e1
hyt <378007599@qq.com>
2024-01-25 14:52:23 +0800

    home screen

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:castleworld/config/Utils.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../widgits/marque_widget.dart';
 
@@ -99,11 +100,11 @@ class _HomeState extends State<Home> {
               child: Stack(
                 clipBehavior: Clip.none,
                 children: [
-                  Text('热门专区',style: TextStyle(color: Color(0xff333333),fontSize: 14,fontWeight: FontWeight.w600),),
+                  Text('热门专区',style: TextStyle(color: Color(0xff333333),fontSize: 14.sp,fontWeight: FontWeight.w600),),
                   Positioned(
                       bottom: -2,
                       left: -2,
-                      child: Image.asset(getImgPath('ic_home_bg',),width: 32,height: 8,fit: BoxFit.fill,))
+                      child: Image.asset(getImgPath('ic_home_bg',),width: 32.w,height: 8.h,fit: BoxFit.fill,))
                 ],
               )),
         );
@@ -113,8 +114,8 @@ class _HomeState extends State<Home> {
     return SliverToBoxAdapter(
           child: Container(
             width: double.infinity,
-            height: 120,
-            padding: EdgeInsets.only(top: 5),
+            height: 120.h,
+            padding: EdgeInsets.only(top: 5.h),
             decoration: BoxDecoration(
               gradient: LinearGradient(colors: [
                 Color(0xff9FFFFC),
@@ -127,7 +128,7 @@ class _HomeState extends State<Home> {
               children: [
                 Container(
                   width: double.infinity,
-                  height: 120,
+                  height: 120.h,
                   child: Builder(
                       builder: (context) {
                         return MarqueeWidget(
@@ -144,7 +145,7 @@ class _HomeState extends State<Home> {
                                         children: d1.map((e) =>
                                             Container(
                                               padding: EdgeInsets.all(2),
-                                              margin: EdgeInsets.only(right: 10,top: 10),
+                                              margin: EdgeInsets.only(right: 10.w,top: 10.h),
                                               decoration: BoxDecoration(
                                                 color: Colors.white,
                                                 borderRadius: BorderRadius
@@ -155,14 +156,14 @@ class _HomeState extends State<Home> {
                                                 children: [
                                                   ClipOval(
                                                       child: Image.network(
-                                                        '${e.img}', width: 22,
-                                                        height: 22,)),
+                                                        '${e.img}', width: 22.w,
+                                                        height: 22.w,)),
                                                   SizedBox(width: 3,),
                                                   Text('${e.title}',
                                                     style: TextStyle(
                                                         color: Color(
                                                             0xff5984B1),
-                                                        fontSize: 12),)
+                                                        fontSize: 12.sp),)
                                                 ],
                                               ),
                                             )).toList()
@@ -189,7 +190,7 @@ class _HomeState extends State<Home> {
   SliverToBoxAdapter buildTap2() {
     return SliverToBoxAdapter(
       child: Container(
-        margin: EdgeInsets.only(left: 14, right: 14, bottom: 16),
+        margin: EdgeInsets.only(left: 14.w, right: 14.w, bottom: 16.h),
         child: Row(
           children: [
             Expanded(
@@ -203,10 +204,10 @@ class _HomeState extends State<Home> {
                 ),
               ),
             ),
-            SizedBox(width: 13,),
+            SizedBox(width: 13.w,),
             Expanded(
               child: Container(
-                height: 85,
+                height: 85.h,
                 decoration: BoxDecoration(
                     image: DecorationImage(
                         image: AssetImage(getImgPath('ic_home_ivn')),
@@ -226,9 +227,9 @@ class _HomeState extends State<Home> {
     return SliverToBoxAdapter(
       child: Container(
         width: double.infinity,
-        height: 165,
-        margin: const EdgeInsets.only(
-            left: 14, right: 14, top: 16, bottom: 10),
+        height: 165.h,
+        margin:  EdgeInsets.only(
+            left: 14.w, right: 14.w, top: 16.h, bottom: 10.h),
         child: Stack(
           children: [
             Positioned(
@@ -236,8 +237,8 @@ class _HomeState extends State<Home> {
               bottom: 0,
               left: 0,
               child: Container(
-                padding: EdgeInsets.only(left: 14, top: 16),
-                width: (size.width / 2) - 14,
+                padding: EdgeInsets.only(left: 14.w, top: 16.h),
+                width: (size.width / 2) - 14.w,
                 decoration: BoxDecoration(
                     image: DecorationImage(
                         image: AssetImage(getImgPath('ic_home_tap_1')),
@@ -249,17 +250,17 @@ class _HomeState extends State<Home> {
                       '购买闪卡',
                       style: TextStyle(
                           fontFamily: 'yf',
-                          fontSize: 30,
+                          fontSize: 30.sp,
                           height: 1.2,
                           color: Colors.white,
                           fontWeight: FontWeight.w400),
                     ),
-                    SizedBox(height: 5,),
+                    SizedBox(height: 5.h,),
                     Text(
                       '开启城堡',
                       style: TextStyle(
 
-                          fontSize: 12,
+                          fontSize: 12.sp,
                           height: 1.3,
                           color: Colors.white,
                           fontWeight: FontWeight.w500),
@@ -276,8 +277,8 @@ class _HomeState extends State<Home> {
               top: 0,
               right: 0,
               child: Container(
-                width: (size.width / 2) - 14 - 1.5,
-                height: 165 / 2 - 5,
+                width: (size.width / 2) - 14.w - 1.5,
+                height: 165.h / 2 - 5,
                 padding: EdgeInsets.only(left: 17, top: 10),
                 decoration: BoxDecoration(
                     image: DecorationImage(image: AssetImage(
@@ -288,13 +289,13 @@ class _HomeState extends State<Home> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('应用大厅', style: TextStyle(fontSize: 24,
+                    Text('应用大厅', style: TextStyle(fontSize: 24.sp,
                         fontFamily: 'yf',
                         color: Colors.white,
                         height: 1.1),),
                     SizedBox(height: 4,),
                     Text('这是一段副文案', style: TextStyle(
-                        fontSize: 12, color: Colors.white, height: 1.2),),
+                        fontSize: 12.sp, color: Colors.white, height: 1.2),),
 
                   ],
                 ),
@@ -309,8 +310,8 @@ class _HomeState extends State<Home> {
                 children: [
                   Container(
                     width: ((size.width / 2)) / 2,
-                    height: 165 / 2 - 5,
-                    padding: EdgeInsets.only(left: 16, top: 7),
+                    height: 165.h / 2 - 5,
+                    padding: EdgeInsets.only(left: 16.w, top: 7.h),
                     decoration: BoxDecoration(
                         image: DecorationImage(
                             image: AssetImage(
@@ -320,12 +321,12 @@ class _HomeState extends State<Home> {
                         )
                     ),
                     child: Text('规则', style: TextStyle(
-                        fontFamily: 'yf', fontSize: 24, color: Colors.white),),
+                        fontFamily: 'yf', fontSize: 24.sp, color: Colors.white),),
                   ),
                   Container(
                     width: ((size.width / 2)) / 2,
-                    height: 165 / 2 - 5,
-                    padding: EdgeInsets.only(left: 16, top: 7),
+                    height: 165.h / 2 - 5,
+                    padding: EdgeInsets.only(left: 16.w, top: 7.h),
                     decoration: BoxDecoration(
                         image: DecorationImage(
                             image: AssetImage(
@@ -335,7 +336,7 @@ class _HomeState extends State<Home> {
                         )
                     ),
                     child: Text('说明', style: TextStyle(
-                        fontFamily: 'yf', fontSize: 24, color: Colors.white),),
+                        fontFamily: 'yf', fontSize: 24.sp, color: Colors.white),),
                   ),
 
 
@@ -352,91 +353,91 @@ class _HomeState extends State<Home> {
   Widget _buildSliverAppBar() {
     return SliverToBoxAdapter(
       child:  SizedBox(
-        height: 300+MediaQuery.of(context).padding.top,
+        height: 300.h+MediaQuery.of(context).padding.top,
         child: Stack(
           children: [
             Image.asset(
               getImgPath('bg_home_homebg'),
               width: double.infinity,
-              height: 211,
+              height: 211.h,
               fit: BoxFit.fill,
             ),
             Column(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 14),
+                  padding:  EdgeInsets.symmetric(horizontal: 14.w),
                   child: Row(
                     children: [
                       Image.asset(
                         getImgPath('ic_home_loc'),
-                        width: 18,
-                        height: 18,
+                        width: 18.w,
+                        height: 18.w,
                       ),
                       SizedBox(
-                        width: 4,
+                        width: 4.w,
                       ),
                       Text(
                         '北京',
                         style:
-                        TextStyle(color: Color(0xff333333), fontSize: 16),
+                        TextStyle(color: Color(0xff333333), fontSize: 16.sp),
                       ),
                       Spacer(),
                       Image.asset(
                         getImgPath('ic_home_kf'),
-                        width: 24,
-                        height: 24,
+                        width: 24.w,
+                        height: 24.w,
                       ),
                       SizedBox(
-                        width: 29,
+                        width: 29.w,
                       ),
                       Image.asset(
                         getImgPath('ic_home_msg'),
-                        width: 24,
-                        height: 24,
+                        width: 24.w,
+                        height: 24.w,
                       ),
                     ],
                   ),
                 ),
                 SizedBox(
-                  height: 20,
+                  height: 20.h,
                 ),
                 Container(
-                  height: 40,
-                  margin: EdgeInsets.symmetric(horizontal: 14),
+                  height: 40.h,
+                  margin: EdgeInsets.symmetric(horizontal: 14.w),
                   decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
+                      borderRadius: BorderRadius.circular(20.r),
                       border: Border.all(color: Color(0xFF444F66), width: 1)),
                   child: Row(
                     children: [
                       SizedBox(
-                        width: 10,
+                        width: 10.w,
                       ),
                       Image.asset(
                         getImgPath('ic_home_srh'),
-                        width: 18,
-                        height: 18,
+                        width: 18.w,
+                        height: 18.w,
                       ),
                       SizedBox(
-                        width: 14,
+                        width: 14.w,
                       ),
                       Text(
                         '请输入搜索内容',
                         style:
-                        TextStyle(color: Color(0xFF444F66), fontSize: 12),
+                        TextStyle(color: Color(0xFF444F66), fontSize: 12.sp),
                       ),
                     ],
                   ),
                 ),
                 SizedBox(
-                  height: 16,
+                  height: 16.h,
                 ),
                 Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 14),
+                  padding:  EdgeInsets.symmetric(horizontal: 14.w),
                   child: Image.asset(
                     getImgPath('bg_home_banner'),
                     width: double.infinity,
-                    height: 162,
+                    height: 162.h,
                     fit: BoxFit.fill,
                   ),
                 ),
