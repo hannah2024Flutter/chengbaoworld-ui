269ebed64d7c2c0da214ff785c1838662ff5870c
hyt <378007599@qq.com>
2024-01-25 14:21:28 +0800

    home

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
             unselectedLabelStyle: TextStyle(fontSize: 9)),
         useMaterial3: true,
       ),
-      home: ProductDetail(),
+      home: AppHome(),
     );
   }
 }
